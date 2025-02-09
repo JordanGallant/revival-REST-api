3cc1eaaf57f7591def51376f8a70cae5c1e2864c
JordanGallant <39-12602@sae.edu.za>
2025-02-09 23:27:58 +0200

    remove cors

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const axios = require("axios");
 const qs = require("qs");
 require("dotenv").config();
 const express = require("express");
-const cors = require("cors");
+
 
 const app = express();
-app.use(cors());
+
 
 
 PORT =3001;
